60ad480ff945a589bb04f434c30b71dd08661b24
pravin maurya <pravinyt1122334455@gmail.com>
2024-02-04 14:48:09 +0545

    finish assignment

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let modal = document.querySelector(".dropdown");
+let servicesButton = document.querySelector(".nav-services");
+let moreButton = document.querySelector(".nav-more");
+let body = document.querySelector("body");
 
+servicesButton.addEventListener("mouseenter", () => {
+  modal.showModal();
+});
+moreButton.addEventListener("mouseenter", () => {
+  modal.showModal();
+});
+modal.addEventListener("click", (e) => {
+  const dialogDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    console.log(e.clientX, dialogDimensions.left);
+    modal.close();
+  }
+});
